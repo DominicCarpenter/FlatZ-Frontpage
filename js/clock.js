386597831780515f5ce29cfe65f7006cefa34b91
Dominic Carpenter <dominicc222@gmail.com>
2015-11-05 13:38:18 +0000

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -11,10 +11,13 @@
         var h = pad(d.getHours());
         var m = pad(d.getMinutes());
         var s = pad(d.getSeconds());
+        var current_date = d.toDateString();
 
         var current_time = [h, m, s].join(':');
 
-        clock.innerHTML = current_time;
+        var clock_date = [current_time, current_date].join("<br>");
+
+        clock.innerHTML = clock_date;
 
     };
 
